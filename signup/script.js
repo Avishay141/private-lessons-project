63208c10b56aeb2047c968ee3181627c562b90ab
guyalmog <guyalmog91@gmail.com>
2020-07-18 15:34:12 +0300

    added signup

--- signup/script.js
+++ signup/script.js
@@ -1,6 +1,59 @@
- $(document).ready(function() {
-   
-      alert("wrong password validation");
-    
+ 
+const  db = firebase.database().ref();
+
+
+$("#sbmt").on("click",function(){
+
+	
+	var email = $("#email").val();
+	var password = $("#Password1").val();
+	var userType = $('input[name=userTypeRadios]:checked').val();
+	//alert("email:"+email+" pass:"+password+ " type:"+userType);
+	var verify = $("#Password2").val();
+
+
+	if(verify != password){
+		alert("wrong password verification");
+		return;
+	} 
+
+  // var userID =  email.split(".").join("*");
+  //
+  //
+  //
+  // var path = "Users/"+ userType +"s/" + userID;
+  //  console.log("db path: " + path);
+  //  //var db = firebase.database().ref();
+  //  db.child(path).set({
+  //    userEmail: email,
+  //    userType: userType
+  //  }).catch(function(error){
+  //    console.log("Error ocurred: ", error);
+  //  });
+
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+      //Registration is successful
+      var user = firebase.auth().currentUser;
+      var userID = user.uid;
+
+      var path = "Users/"+ userType +"s/" + userID;
+       console.log("db path: " + path);
+       //var db = firebase.database().ref();
+       db.child(path).set({
+         userEmail: email,
+         userType: userType
+       }).catch(function(error){
+         console.log("Error ocurred: ", error);
+       });
+      console.log("user created successfuly");
+
+
+
+      }).catch(e => console.log(e.message));
 
   });
+
+     
+    
+
