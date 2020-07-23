c88eee71bd4970f27d5b7568b43801099ee25e95
nadavmg7 <Avishay141@gmail.com>
2020-07-23 18:40:23 +0300

    search done, profile button added

--- main/main.js
+++ main/main.js
@@ -24,7 +24,9 @@ $("#edit_teacher_profile").on("click", function () {
 });
 
 
-
+$("#searchBtn").on("click", function (event) {
+  db.ref("Users/Teachers/").on("value", read);
+});
 
 
 function read(data) {
@@ -32,72 +34,54 @@ function read(data) {
   var selected_Language = $("#ddl_language").children("option:selected").val();
   var selected_Origin = $("#ddl_origin").children("option:selected").val();
   var selected_Price = document.getElementById("rangeSlider").value;
-  console.log("requested language: " + selected_Language);
-  console.log("requested origin: " + selected_Origin);
-  console.log("requested price: " + selected_Price);
-  console.log("search results: ");
 
-
+  $(".profiles").empty();
   var teachers = data.val();
   var teachers_ids = Object.keys(teachers);
   var res = [];
   var res_size = 0;
   for (var i = 0; i < teachers_ids.length; i++) {
     id = teachers_ids[i];
-
     if (teachers[id].languages_I_teach && teachers[id].lessonPrice && teachers[id].homeCountry) {
-      console.log("a");
-      console.log(teachers[id].lessonPrice <= selected_Price);
-      console.log(teachers[id].homeCountry == selected_Origin);
       if (search_language(teachers[id].languages_I_teach, selected_Language) &&
-        teachers[id].lessonPrice <= selected_Price &&
+        parseInt(teachers[id].lessonPrice) <= parseInt(selected_Price) &&
         teachers[id].homeCountry == selected_Origin) {
-        res.push({ name: teachers[id].name, email: teachers[id].userEmail });
-        console.log("res item number: " + res_size + " name:" + res[res_size].name + " email: " + res[res_size].email + " \n");
+        res.push({ name: teachers[id].name, email: teachers[id].userEmail , teacherId: id});
         res_size++;
       }
     }
   }
-
-
-  update_html_profiles(teachers[id].name, teachers[id].userEmail, res_size);
-
-
+  update_html_profiles(res, res_size);
 }
 
 function search_language(languages, selected_Language) {
   for (var i = 0; i < languages.length; i++) {
-    if (languages[i] == selected_Language) {
-      console.log("b");
+    if (languages[i] == selected_Language) 
       return true;
-    }
   }
   return false;
 }
 
-
-$("#searchBtn").on("click", function (event) {
-  db.ref("Users/Teachers/").on("value", read);
-});
-
-
-function update_html_profiles(name, email, res_size) {
-  $(".profiles").empty();
-
-  for (var x = 0; x < res_size; x++) {
-    var html_string = "<div class=\"card\" style=\"width:250px\" imgstyle= >"
-      + "<img class=\"card-img-top\" src=\"https://www.w3schools.com/bootstrap4/img_avatar1.png\" alt=\"Card image\" style=\"width:100%\">"
-      + "<div class=\"card-body\">"
-      + "<h4 class=\"card-title\">" + name + "</h4>"
-      + "<p class=\"card-text\">Email:" + email + " \n</p>"
-      + "<a href=\"#\" class=\"btn btn-primary\">See Profile</a>"
-      + "</div>"
-      + "</div>";
-    $(".profiles").append(html_string);
-
+function update_html_profiles(res, res_size) {
+  
+  console.log(" was printed : " + name + "   " + res_size);
+  
+    for (var x = 0; x < res_size; x++) {
+      var html_string = "<div class=\"card\" style=\"width:250px\" imgstyle= >"
+        + "<img class=\"card-img-top\" src=\"https://www.w3schools.com/bootstrap4/img_avatar1.png\" alt=\"Card image\" style=\"width:100%\">"
+        + "<div class=\"card-body\">"
+        + "<h4 class=\"card-title\">" + res[x].name + "</h4>"
+        + "<p class=\"card-text\">Email:" + res[x].email + " \n</p>"
+        + "<a href=\"#\" id="+ res.teacherId +" onclick=\"profile_btn_func(this.id)\" class=\"btn btn-primary\">See Profile</a>"
+        + "</div>"
+        + "</div>";
+      $(".profiles").append(html_string);
+    }
   }
 
-}
+  function profile_btn_func(teacherID){
+    window.location = "../teacher_info/teacher_info.html?uid=" + teacherID;
+  }
 
 
 
