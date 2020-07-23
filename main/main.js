b559ad420b61f09297e3a497ada781bb06639b34
nadavmg7 <Avishay141@gmail.com>
2020-07-23 15:29:06 +0300

    search issues

--- main/main.js
+++ main/main.js
@@ -20,7 +20,7 @@ range.addEventListener('input', setValue);
 
 /////////////---------- Read Teachers for search
 $("#edit_teacher_profile").on("click", function () {
-  window.location = "../teacher_page/teacher.html?uid="+user.uid;
+  window.location = "../teacher_page/teacher.html?uid=" + userID;
 });
 
 
@@ -44,27 +44,31 @@ function read(data) {
   var res_size = 0;
   for (var i = 0; i < teachers_ids.length; i++) {
     id = teachers_ids[i];
-    
-    if (teachers[id].languages_I_teach && teachers[id].lessonPrice && teachers[id].homeCountry)
-      if (search_language(teachers[id].languages_I_teach,selected_Language) && 
+
+    if (teachers[id].languages_I_teach && teachers[id].lessonPrice && teachers[id].homeCountry) {
+      console.log("a");
+      console.log(teachers[id].lessonPrice <= selected_Price);
+      console.log(teachers[id].homeCountry == selected_Origin);
+      if (search_language(teachers[id].languages_I_teach, selected_Language) &&
         teachers[id].lessonPrice <= selected_Price &&
-        teachers[id].homeCountry == selected_Origin) 
-        {
-        
+        teachers[id].homeCountry == selected_Origin) {
         res.push({ name: teachers[id].name, email: teachers[id].userEmail });
         console.log("res item number: " + res_size + " name:" + res[res_size].name + " email: " + res[res_size].email + " \n");
         res_size++;
       }
+    }
   }
 
-  update_html_profiles(teachers[id].name,teachers[id].userEmail,res_size);
+
+  update_html_profiles(teachers[id].name, teachers[id].userEmail, res_size);
 
 
 }
 
-function search_language(languages,selected_Language){
-  for(var i=0; i<languages.length;i++){
-    if(languages[i]==selected_Language){
+function search_language(languages, selected_Language) {
+  for (var i = 0; i < languages.length; i++) {
+    if (languages[i] == selected_Language) {
+      console.log("b");
       return true;
     }
   }
@@ -77,29 +81,28 @@ $("#searchBtn").on("click", function (event) {
 });
 
 
-function update_html_profiles(name, email,res_size){
+function update_html_profiles(name, email, res_size) {
   $(".profiles").empty();
- 
+
   for (var x = 0; x < res_size; x++) {
     var html_string = "<div class=\"card\" style=\"width:250px\" imgstyle= >"
       + "<img class=\"card-img-top\" src=\"https://www.w3schools.com/bootstrap4/img_avatar1.png\" alt=\"Card image\" style=\"width:100%\">"
       + "<div class=\"card-body\">"
       + "<h4 class=\"card-title\">" + name + "</h4>"
-      + "<p class=\"card-text\">Email:" + email +  " \n</p>"
+      + "<p class=\"card-text\">Email:" + email + " \n</p>"
       + "<a href=\"#\" class=\"btn btn-primary\">See Profile</a>"
       + "</div>"
       + "</div>";
-      $(".profiles").append(html_string);
-    
+    $(".profiles").append(html_string);
+
   }
-  
-  //$(".profiles1").append(html_string);
+
 }
 
 
 
 /*
-var userID;
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (!user) {
 
@@ -111,12 +114,12 @@ firebase.auth().onAuthStateChanged(function(user) {
 });*/
 
 
-function get_userID_from_url(){
+function get_userID_from_url() {
   var res = location.search.substring(1).split("=")[1];
   console.log("userID from url: " + res);
   return res;
 }
 
-$("#logout_btn").on("click",function(){
+$("#logout_btn").on("click", function () {
   firebase.auth().signOut();
 });
